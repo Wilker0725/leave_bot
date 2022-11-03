6758c242b2564e975ecc78f03a4e64c3e8aeece2
Wilker <wilker.soh@cognizant.com>
2022-11-03 13:53:10 +0800

    Reuse Search Component

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,59 +1,58 @@
-import NextLink from "next/link"
-import Search from "@/components/Search/Search"
-import UsersList from "@/features/users/UsersList"
-import { IconButton, Stack } from "@mui/material"
-import Drawer from "@mui/material/Drawer"
-import Link from "@mui/material/Link"
-import Divider from "@mui/material/Divider"
-import AddIcon from '@mui/icons-material/Add';
-import { FormEvent, useState } from "react"
-import Container from "@mui/material/Container"
-import { GridSearchIcon } from "@mui/x-data-grid"
-import { useAppDispatch } from "@/app/store"
+import NextLink from "next/link";
+import UsersList from "@/features/users/UsersList";
+import { IconButton, Stack } from "@mui/material";
+import Drawer from "@mui/material/Drawer";
+import Link from "@mui/material/Link";
+import AddIcon from "@mui/icons-material/Add";
+import { FormEvent, useState } from "react";
+import Container from "@mui/material/Container";
+import { GridSearchIcon } from "@mui/x-data-grid";
+import { useAppDispatch } from "@/app/store";
 import {
   setUserPageQuery,
   setUserSearch,
   resetQuery,
   setUserPage,
-} from "@/features/users/userSlice"
-import { objectToQuery } from "@/utils/queryTransform"
-import RestartAltIcon from "@mui/icons-material/RestartAlt"
+} from "@/features/users/userSlice";
+import { objectToQuery } from "@/utils/queryTransform";
+import RestartAltIcon from "@mui/icons-material/RestartAlt";
+import SearchUser from "@/features/users/SearchUser";
 
 type typeFormData = {
-  projectName?: string
-  employeeName?: string
-  employeeId?: string
-  role?: string
-}
+  projectName?: string;
+  employeeName?: string;
+  employeeId?: string;
+  role?: string;
+};
 
 const User = () => {
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
 
-  const [formData, setFormData] = useState<typeFormData | object>({})
-  const [isSearch, setIsSearch] = useState<boolean>(false)
-  const [isSearchOpen, setIsSearchOpen] = useState<boolean>(false)
+  const [formData, setFormData] = useState<typeFormData | object>({});
+  const [isSearch, setIsSearch] = useState<boolean>(false);
+  const [isSearchOpen, setIsSearchOpen] = useState<boolean>(false);
 
   const handleSearchInput = (
     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
-    const name = event.target.id
-    setFormData({ ...formData, [name]: event.target.value.trim() })
-  }
+    const name = event.target.id;
+    setFormData({ ...formData, [name]: event.target.value.trim() });
+  };
 
   const handleOnSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const userQuery = objectToQuery(formData)
+    event.preventDefault();
+    const userQuery = objectToQuery(formData);
 
-    dispatch(setUserSearch(userQuery))
-    dispatch(setUserPage(0))
-    dispatch(setUserPageQuery(`page=1&limit=10&${userQuery}`))
+    dispatch(setUserSearch(userQuery));
+    dispatch(setUserPage(0));
+    dispatch(setUserPageQuery(`page=1&limit=10&${userQuery}`));
 
-    setIsSearch(true)
+    setIsSearch(true);
 
     // reset state
-    setIsSearchOpen(false)
-    setFormData({})
-  }
+    setIsSearchOpen(false);
+    setFormData({});
+  };
 
   const toggleDrawer =
     (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
@@ -62,11 +61,11 @@ const User = () => {
         ((event as React.KeyboardEvent).key === "Tab" ||
           (event as React.KeyboardEvent).key === "Shift")
       ) {
-        return
+        return;
       }
 
-      setIsSearchOpen(open)
-    }
+      setIsSearchOpen(open);
+    };
 
   return (
     <Container>
@@ -80,8 +79,8 @@ const User = () => {
               color={"info"}
               style={{ paddingLeft: 0 }}
               onClick={() => {
-                dispatch(resetQuery({}))
-                setIsSearch(false)
+                dispatch(resetQuery({}));
+                setIsSearch(false);
               }}
             >
               <RestartAltIcon />
@@ -100,12 +99,15 @@ const User = () => {
           open={isSearchOpen}
           onClose={toggleDrawer(false)}
         >
-          <Search onChangeText={handleSearchInput} onSubmit={handleOnSubmit} />
+          <SearchUser
+            onChangeText={handleSearchInput}
+            onSubmit={handleOnSubmit}
+          />
         </Drawer>
         <UsersList />
       </Stack>
     </Container>
-  )
-}
+  );
+};
 
-export default User
+export default User;
